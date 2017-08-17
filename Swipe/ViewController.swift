9b0ae17b39248476487ad3b84c2bfad7a1cd62f0
Renato Degiovanni <rdgvnn@gmail.com>
2017-08-17 18:56:05 -0300

    SwipeExample
    Swiping Example made with Swift

--- Swipe/ViewController.swift
+++ Swipe/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(swipeAction(swipe:)))
+        leftSwipe.direction = UISwipeGestureRecognizerDirection.left
+        self.view.addGestureRecognizer(leftSwipe)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
+extension UIViewController {
+    func swipeAction(swipe:UISwipeGestureRecognizer)
+    {
+        switch swipe.direction.rawValue {
+        case 1:
+            performSegue(withIdentifier: "goLeft", sender: self)
+        case 2:
+            performSegue(withIdentifier: "goRight", sender: self)
+        default:
+            break
+        }
+    }
+}
+
+
 
